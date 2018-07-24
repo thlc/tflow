3fa8f1e9efcc11081d4ec2b2e53716a068d4a27b
Thomas Lecomte <th.lecomte@gmail.com>
2018-07-24 15:29:15 +0200

    initializing...

--- tflow.py
+++ tflow.py
@@ -6,38 +6,62 @@ import argparse
 import tempfile
 import sys
 import os
+import sqlite3
 
 baseurl = "http://diffusion-numerique.info-routiere.gouv.fr/tipitrafic/TraficMarius/"
 
-def init_workdir():
+def die(err):
+    print err
+    sys.exit(42)
 
+def log(level, msg):
+    print msg
+
+def init_workdir(args):
     # sqlite + RRD init
+    if os.path.isdir(args.workdir):
+        die("fatal: workdir already exists")
+    os.mkdir(args.workdir) and log(DEBUG, "created workdir %s" % args.workdir)
+    os.mkdir(args.workdir + '/rrd') and log (DEBUG, "created rrd subdir")
+    conn = sqlite3.connect(args.workdir + '/tflow.db')
+    c = conn.cursor()
+    c.execute('''CREATE TABLE timestamp (last DATETIME)''')
+    conn.commit()
+    conn.close()
+    log(DEBUG, "created SQLite database successfully")
+
+def init_rrd(args, sensor_name):
+    # init args.workdir+"/rrd/"+${sensor_name}.rrd
+    True
 
 def fetch_data():
-
     # fetch the stuff
+    True
 
-def process_file()
-
+def process_file():
     # process a single DATEX2 file into RRD
+    True
 
 
 def main():
-    parser = argparse.ArguemntParser(description='Traffic data analyser')
+    parser = argparse.ArgumentParser(description='Traffic data analyser')
     parser.add_argument('-u', '--user', help='username', required=True, type=str, action='store')
     parser.add_argument('-p', '--password', help='password', required=True, type=str, action='store')
-    parser.add_arguemnt('-d', '--workdir', default='./data', type=str, action='store')
-    parser.add_argument('-I', '--init', type=str, action='store')
-    parser.add_argument('-C', '--catchup', help='catchup with old data')
+    parser.add_argument('-d', '--workdir', default='./data', type=str, action='store')
+    parser.add_argument('-I', '--init', action='store_true')
+    parser.add_argument('-C', '--catchup', help='catchup with old data', action='store_true')
 
     args = parser.parse_args()
 
     if args.init:
-        init_workdir(args.workdir)
+        init_workdir(args)
         sys.exit(0)
 
-    if not os.isdir(args.workdir):
+    if not os.path.isdir(args.workdir):
         log("error: %s doesn't exist" % args.workdir)
         sys.exit(1)
 
     fetch_data(parser)
+
+if __name__ == "__main__":
+    main()
