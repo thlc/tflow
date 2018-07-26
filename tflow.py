13cf1a8e0f83d06445f5e3359385350e2628ed88
Thomas Lecomte <th.lecomte@gmail.com>
2018-07-26 14:53:23 +0200

    feeds RRD and graph stuff

--- tflow.py
+++ tflow.py
@@ -10,12 +10,25 @@ import sqlite3
 import requests
 import re
 import time
+import rrdtool
 import xml.dom.minidom
+import strict_rfc3339
+
 from xml.dom.minidom import parse
 from time import mktime
 from datetime import datetime
 from dateutil.parser import parse
 from bs4 import BeautifulSoup
+
+# quick'n'dirty mapping for marseille
+mapping=[ ('4', 'A50'),
+          ('3', 'A50TC' ), # tronc commun (aubagne-marseille)
+          ('1', 'A51' ),
+          ('5', 'A55' ),
+          ('7', 'A7S' ), # A7 jusqu'a ech A51
+          ('8', 'A7TC' ), # A7 jusqu'a saint charles
+          ('2', 'A507' ) # L2
+        ]
 
 # loglevels
 DEBUG    = 3
@@ -34,24 +47,22 @@ def die(err):
 def log(level, msg):
     print(msg)
 
-def init_workdir(args):
+def init_workdir():
     # sqlite + RRD init
     if os.path.isdir(args.workdir):
         die("fatal: workdir already exists")
     os.mkdir(args.workdir) and log(DEBUG, "created workdir %s" % args.workdir)
     os.mkdir(args.workdir + '/rrd') and log (DEBUG, "created rrd subdir")
+    os.mkdir(args.workdir + '/graphs')
     conn = sqlite3.connect(args.workdir + '/tflow.db')
     c = conn.cursor()
-    c.execute('''CREATE TABLE timestamp (last DATETIME)''')
-    c.execute('''INSERT INTO timestamp (last) VALUES (DATETIME('1970-01-01 00:00:00'))''')
+    c.execute('''CREATE TABLE timestamp (last INTEGER)''')
+    c.execute('''INSERT INTO timestamp (last) VALUES (0)''')
     conn.commit()
     conn.close()
     log(DEBUG, "created SQLite database successfully")
 
-def init_rrd(args, sensor_name):
-    # init args.workdir+"/rrd/"+${sensor_name}.rrd
-    True
-
+# FIXME maintain one connection for the whole run
 
 # get the last timestamp that we processed
 def get_last_ts():
@@ -61,15 +72,55 @@ def get_last_ts():
     r = c.fetchone()
     conn.commit()
     conn.close()
-    return parse(r[0])
+    return int(r[0])
+
+def update_last_ts(ts):
+    conn = sqlite3.connect(args.workdir + '/tflow.db')
+    c = conn.cursor()
+    c.execute("UPDATE timestamp SET last = %s" % str(ts))
+    conn.commit()
+    conn.close()
+
+def init_rrd(path, ts_start):
+    rrdtool.create(path, '-b', ts_start,
+                        '-s', '360',
+                        'DS:vehicleFlow:GAUGE:360:0:10000',
+                        'DS:occupancy:GAUGE:360:0:100',
+                        'DS:speed:GAUGE:360:0:200',
+                        'RRA:AVERAGE:0.8:1:240',
+                        'RRA:AVERAGE:0.8:10:8100')
+
+def update_rrd(sensor, ts, flow, occupancy, speed):
+    rrdfilepath = args.workdir + '/rrd/' + sensor + '.rrd'
+    if not os.path.isfile(rrdfilepath):
+        # (minimum one second step)
+        init_rrd(rrdfilepath, str(int(ts) - 1))
+    log(DEBUG, "[%s] inserting data (%s,%s,%s) for ts %s" % (sensor, flow, occupancy, speed, ts))
+    rrdtool.update(rrdfilepath, "%s:%s:%s:%s" % (ts, flow, occupancy, speed)) 
 
 def process_measurement(m, ts):
     sensor = m.getElementsByTagName('predefinedLocationReference')[0].getAttribute('id')
-    flow = m.getElementsByTagName('vehicleFlowRate')[0].firstChild.data
-    occupancy = m.getElementsByTagName('percentage')[0].firstChild.data 
-    speed = m.getElementsByTagName('speed')[0].firstChild.data 
+    flow = int(m.getElementsByTagName('vehicleFlowRate')[0].firstChild.data)
+    occupancy = float(m.getElementsByTagName('percentage')[0].firstChild.data)
+    speed = float(m.getElementsByTagName('speed')[0].firstChild.data)
 
-    print('sensor: %s flow: %s  occ: %s  speed: %s' % (sensor, flow, occupancy, speed))
+    s_prettyfwy = '<unknown>'
+
+    s = re.search('([A-Za-z]{2})([0-9])([0-9]{2})\.([a-zA-Z][0-9])', sensor)
+    if s == None:
+        print("unmatched %s" % sensor)
+    else:
+        s_region = s.group(1)
+        s_fwy = s.group(2)
+        s_dept = s.group(3)
+        s_sid = s.group(4)
+
+        for m in mapping:
+            if m[0] == s_fwy:
+                s_prettyfwy = m[1]
+
+    #print('sensor: %s (%s) flow: %i  occ: %f  speed: %f' % (sensor, s_prettyfwy, flow, occupancy, speed))
+    update_rrd(sensor, ts, flow, occupancy, speed)
 
 # process a single DATEX2 file into RRD
 def process_file(file):
@@ -77,10 +128,11 @@ def process_file(file):
 
     pub = dom.getElementsByTagName('payloadPublication')[0]
     pubtime = (pub.getElementsByTagName('publicationTime'))[0].firstChild.data
+    ts = str(int(strict_rfc3339.rfc3339_to_timestamp(pubtime)))
     print("pubtime: %s" % pubtime)
     for child in pub.childNodes:
         if child.nodeType == child.ELEMENT_NODE and child.tagName == 'siteMeasurements':
-            process_measurement(child, pubtime)
+            process_measurement(child, ts)
 
     True
 
@@ -88,7 +140,7 @@ def fetch_sixmin(sixmin_url):
     print("working on " + sixmin_url)
     xml = requests.get(sixmin_url, auth=(args.user, args.password)).text
     process_file(xml)
-    sys.exit(0) # tmp
+    #sys.exit(0) # tmp
 
 def fetch_day(url, last_ts):
     print("working on " + url)
@@ -99,7 +151,7 @@ def fetch_day(url, last_ts):
             continue
         else:
             ts = m.group(1)
-            parsed_ts = datetime.fromtimestamp(mktime(time.strptime(ts, "%Y%m%d_%H%M%S")))
+            parsed_ts = int(mktime(time.strptime(ts, "%Y%m%d_%H%M%S")))
             if parsed_ts > last_ts:
                 fetch_sixmin(sixmin)
             else:
@@ -119,10 +171,11 @@ def fetch_data(network = 'TraficMarius'):
             continue
         else:
             day_ts = m.group(1)
-            # 2018-07-25_09
-            parsed_ts = datetime.fromtimestamp(mktime(time.strptime(day_ts, '%Y-%m-%d_%H')))
+            # 2018-07-25_09 - we append the minute '59' to get the whole hour
+            parsed_ts = int(mktime(time.strptime(day_ts + '-59', '%Y-%m-%d_%H-%M')))
             if parsed_ts > last_ts:
                 fetch_day(day, last_ts)
+                update_last_ts(parsed_ts)
             else:
                 print("skipping " + day_ts)
 
@@ -134,6 +187,58 @@ def get_page_contents(url, ext):
     soup = BeautifulSoup(page, 'html.parser')
     return [ url + '/' + node.get('href') for node in soup.find_all('a') if node.get('href').endswith(ext) ]
 
+def draw_graph(rrdfile):
+    m = re.search('(.*)\.rrd$', os.path.basename(rrdfile))
+    sensor_name = m.group(1)
+    g_range = '2d'
+    height = '200'
+    width = '400'
+    scale = 2
+    rrdtool.graph(args.workdir + "/graphs/%s.png" % sensor_name,
+                    '--end', 'now', '--start', "end-%s" % g_range,
+                    '-E', '-N', '-h', height, '-l', '0', '-t', sensor_name,
+                    '-v', 'veh/h', '-X', '0', '-T' '20', '--right-axis', "%s:0" % scale,
+                    '--right-axis-label', 'km/h | %',
+		    "DEF:vehicleFlow=%s:vehicleFlow:AVERAGE:step=360" % rrdfile,
+		    "DEF:speed=%s:speed:AVERAGE:step=360" % rrdfile,
+                    "DEF:occupancy=%s:occupancy:AVERAGE:step=360" % rrdfile,
+		    'CDEF:scaledFlow=vehicleFlow,10,*',
+		    "CDEF:scaledSpeed=speed,%s,/" % scale,
+                    "CDEF:scaledOccupancy=occupancy,%s,/" % scale,
+		    'VDEF:vehicleAvg=vehicleFlow,AVERAGE',
+		    'VDEF:vehicleMin=vehicleFlow,MINIMUM',
+		    'VDEF:vehicleMax=vehicleFlow,MAXIMUM',
+		    'VDEF:vehicleLast=vehicleFlow,LAST',
+		    'VDEF:speedAvg=speed,AVERAGE',
+		    'VDEF:speedMin=speed,MINIMUM',
+		    'VDEF:speedMax=speed,MAXIMUM',
+		    'VDEF:speedLast=speed,LAST',
+		    'VDEF:occupancyAvg=occupancy,AVERAGE',
+		    'VDEF:occupancyMin=occupancy,MINIMUM',
+		    'VDEF:occupancyMax=occupancy,MAXIMUM',
+		    'VDEF:occupancyLast=occupancy,LAST',
+		    'LINE1:scaledFlow#0000FF:"vehicleFlow    "',
+		    'LINE1:scaledSpeed#FF0000:"speed    "',
+		    'LINE1:scaledOccupancy#00FF00:"occupancy    \c"',
+		    'GPRINT:vehicleAvg:"avg %4.0lf0 veh/h\t"',
+		    'GPRINT:vehicleMin:"min %4.0lf0 veh/h\t"',
+		    'GPRINT:vehicleMax:"max %4.0lf0 veh/h\t"',
+		    'GPRINT:vehicleLast:"last %4.0lf0 veh/h\l"',
+		    'GPRINT:speedAvg:"avg %4.0lf km/h \t"',
+		    'GPRINT:speedMin:"min %4.0lf km/h \t"',
+		    'GPRINT:speedMax:"max %4.0lf km/h \t"',
+		    'GPRINT:speedLast:"last %4.0lf km/h \l"',
+		    'GPRINT:occupancyAvg:"avg %4.0lf %%\t"',
+		    'GPRINT:occupancyMin:"min %4.0lf %%\t"',
+		    'GPRINT:occupancyMax:"max %4.0lf %%\t"',
+		    'GPRINT:occupancyLast:"last %4.0lf %%      "')
+
+def draw_graphs():
+    for filename in os.listdir(args.workdir + '/rrd'):
+        if filename.endswith('.rrd'):
+            draw_graph(args.workdir + '/rrd/' + filename)
+        else:
+            continue
 
 
 def main():
@@ -143,6 +248,7 @@ def main():
     parser.add_argument('-d', '--workdir', default='./data', type=str, action='store')
     parser.add_argument('-I', '--init', action='store_true')
     parser.add_argument('-C', '--catchup', help='catchup with old data', action='store_true')
+    parser.add_argument('-g', '--graph', help='draw the graph', action='store_true')
 
     global args
     args = parser.parse_args()
@@ -155,7 +261,10 @@ def main():
         log("error: %s doesn't exist" % args.workdir)
         sys.exit(1)
 
-    fetch_data()
+    if args.graph:
+        draw_graphs()
+    else:
+        fetch_data()
 
 if __name__ == "__main__":
     main()
